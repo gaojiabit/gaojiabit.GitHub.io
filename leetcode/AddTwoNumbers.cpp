b9f56a96d58b0349816c6f266677007f0e890bf6
gaojiabit <15011337033@139.com>
2021-12-20 11:37:07 +0800

    addtwonumbers

--- leetcode/AddTwoNumbers.cpp
+++ leetcode/AddTwoNumbers.cpp
@@ -2,29 +2,23 @@
 
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-    ~ListNode() {
-        while (this != nullptr)
-        {
-            ListNode* p = this;
-            this = this->next;
-            free(p);
-        }
-    }
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+
+
 };
 
 ListNode* addTwoNumbers(ListNode* numa, ListNode* numb)
 {
-    ListNode* resultIterator = nullptr;
-    ListNode* result = resultIterator;
+    ListNode* result = new ListNode();
+    ListNode* curr = result;
     int overflow = 0;
     int ret = 0;
     while (true)
     {
-        if (numa == nullptr && numb == nullptr)
+        if (numa == nullptr && numb == nullptr && overflow == 0)
             break;
         if (numa != nullptr)
         {
@@ -41,29 +35,51 @@ ListNode* addTwoNumbers(ListNode* numa, ListNode* numb)
             ret += overflow;
             overflow = 0;
         }
+
         overflow = ret / 10;
         ret = ret % 10;
-        resultIterator = new ListNode(ret);
+        curr->next = new ListNode(ret);
         ret = 0;
-        resultIterator = resultIterator->next;
+        curr = curr->next;
     }
-    return result;
+    ListNode* retp = result->next;
+    delete result;
+    return retp;
+
+
 }
 
 
 int main()
 {
-    ListNode c0(0);
-    ListNode c1(1);
-    ListNode c2(2);
-    ListNode c3(3);
-    ListNode c4(4);
-    ListNode c5(5);
-    ListNode a(0);
-    ListNode b(0);
-    ListNode* ret = nullptr;
+    ListNode* p0 = new ListNode(0);
+    ListNode* p1 = new ListNode(1);
+    ListNode* p2 = new ListNode(2);
+    ListNode* p3 = new ListNode(3);
+    ListNode* p4 = new ListNode(4);
+    ListNode* p5 = new ListNode(5);
+    ListNode* p6 = new ListNode(6);
+    ListNode* a = new ListNode(0);
+    a->next = p5;
+    p5->next = p3;
+    p3->next = p1;
+    ListNode* b = new ListNode(0);
+    b->next = p6;
+    p6->next = p4;
+    p4->next = p2;
 
-    ret = addTwoNumbers(&a, &b);
+
+    ListNode* ret = addTwoNumbers(a, b);
+
+    delete p0;
+    delete p1;
+    delete p2;
+    delete p3;
+    delete p4;
+    delete p5;
+    delete p6;
+    delete a;
+    delete b;
 
 
 
